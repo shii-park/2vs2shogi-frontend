4eb0cb6861351792bec1eed78eadabc66b4fab64
meg44k <yugomegane@gmail.com>
2025-12-20 21:57:45 +0900

    Add: ルールのUIを変更しました

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -4,9 +4,21 @@ export default function Page() {
   return (
     <div className={style.container}>
       <div className={style.title}>遊び方</div>
-      <ul className={style.ulHowTo}>
-        <li>完全ランダムマッチング！</li>
-      </ul>
+      
+      <div className={style.rulesContainer}>
+        <div className={style.ruleBox}>
+          <h2>ルール1</h2>
+          <p>ここにルールの説明が入ります。</p>
+        </div>
+        <div className={style.ruleBox}>
+          <h2>ルール2</h2>
+          <p>ここにルールの説明が入ります。</p>
+        </div>
+        <div className={style.ruleBox}>
+          <h2>ルール3</h2>
+          <p>ここにルールの説明が入ります。</p>
+        </div>
+      </div>
     </div>
   )
 }
