e101befa325e7e98a89396f23283ec980e09b819
meg44k <yugomegane@gmail.com>
2025-12-20 20:36:58 +0900

    Add: ルールページに初期の配置を追加しました

--- app/rules/page.tsx
+++ app/rules/page.tsx
@@ -1,3 +1,12 @@
+import style from "./page.module.css"
+
 export default function Page() {
-  return <div>tmp</div>;
+  return (
+    <div className={style.container}>
+      <div className={style.title}>遊び方</div>
+      <ul className={style.ulHowTo}>
+        <li>完全ランダムマッチング！</li>
+      </ul>
+    </div>
+  )
 }
