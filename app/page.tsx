6c88960efcf49f5aaf54a758b3e95bce28557f01
ohgushi0329 <k21033ko@apps.kct.ac.jp>
2025-12-21 00:40:07 +0900

    Fix:httpリクエストをGETからPOSTに変更しました

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Page() {
 
     try { 
       // バックのAPIをたたく
-      const respone = await fetch(`http://localhost:8080/api/auth/register?username=${encodeURIComponent(userName)}`);
+      const respone = await fetch(`http://localhost:8080/api/auth/register?username=${encodeURIComponent(userName)}`,{method: 'POST'});
 
       // リクエストエラー処理
       if (!respone.ok) { throw new Error("登録に失敗しました"); }
