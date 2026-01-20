7be8ace4ae8926988559545f22a005562671e5d1
Suiren91 <rento8191@gmail.com>
2026-01-20 16:45:06 +0900

    WebSocketをwssからws二変更

--- hooks/useSocket.tsx
+++ hooks/useSocket.tsx
@@ -27,7 +27,7 @@ export const SocketProvider = ({children}: { children: ReactNode}) => {
     // ソケット接続フラグ : コネクションを条件付きで開始するため
     const  [shouldConnect, setShouldConnect] = useState(false);
 
-    const WS_BASE_URL = 'wss://localhost:8080';
+    const WS_BASE_URL = 'ws://localhost:8080';
 
 
     // 接続時に送信するためユーザー名取得
